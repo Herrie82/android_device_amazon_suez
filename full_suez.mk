45743e3141d2ee1e78b8c63e3711722e68af8630
Herrie <Github.com@herrie.org>
2021-05-17 18:46:47 +0200

    full_suez.mk: Minimize Android build for Halium 9.0

--- full_suez.mk
+++ full_suez.mk
@@ -1,7 +1,7 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/amazon/suez/device.mk)
